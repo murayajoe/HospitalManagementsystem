931d2b916c948aab1d42ba46efb3d465747cdc6b
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-06 07:15:06 +0300

    1st

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Hospital Management System',
+    'version': '18.0',
+    'summary': 'Module for managing hospital operations',
+    'sequence': -100,
+    'description': """
+        A comprehensive Hospital Management System for managing patients, doctors,
+        appointments, medical records, billing, and more.
+    """,
+    'category': 'Healthcare',
+    'author': 'Joe Muraya',
+    'maintainer': 'Joe Muraya',
+    'website': 'https://clickpoa.odoo.com',
+    'license': 'LGPL-3',
+    'depends': [],
+    'data': [],
+    'assets': {},
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ models/patient.py
@@ -0,0 +1,5 @@
+from odoo import api, fields, models
+class HospitalPatient(models.model):
+_name = 'hospital.patient'
+
+    
