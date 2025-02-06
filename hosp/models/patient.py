372c589f3d29f5fd2593b4172cd95f5f0cc1a291
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-06 07:22:58 +0300

    1

--- hosp/models/patient.py
+++ hosp/models/patient.py
@@ -1,5 +1,9 @@
-from odoo import api, fields, models
-class HospitalPatient(models.model):
-_name = 'hospital.patient'
+from odoo import models, fields
 
-    
+class Patient(models.Model):
+    _name = 'hospital.patient'
+    _description = 'Hospital Patient'
+
+    name = fields.Char(string='Patient Name', required=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
