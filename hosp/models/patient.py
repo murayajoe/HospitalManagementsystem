e04cb66ac2d0266eac59ef1748c5ef5ac0086116
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-17 14:52:13 +0300

    kanban

--- hosp/models/patient.py
+++ hosp/models/patient.py
@@ -15,6 +15,7 @@ class HospitalPatient(models.Model):
     insurance_number = fields.Integer(string="Insurance Number")
     weight = fields.Float(string="Weight")
     active = fields.Boolean(string="Active", default='True')
+    # doctor = fields.Boolean(sting="Active" ,default='True')
 # class FemalePatient(models.Model):
 #     _name = 'female.patient'
 
