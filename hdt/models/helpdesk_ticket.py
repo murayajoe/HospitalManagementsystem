2c46fc9725fdadb735479228312300feb161b522
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-17 11:40:32 +0300

    del hdt

--- hdt/models/helpdesk_ticket.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class HelpdeskTicket(models.Model):
-    _inherit = 'helpdesk.ticket'
-
-    related_ticket_ids = fields.Many2many(
-        'helpdesk.ticket', 'helpdesk_ticket_rel', 'ticket_id', 'related_ticket_id', 
-        string='Related Tickets'
-    )
