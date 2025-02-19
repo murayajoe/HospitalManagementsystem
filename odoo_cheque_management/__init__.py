6ddf29df9cfe3ef4f1f8bcf0fbd28bc06e1fb0c8
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-19 12:30:14 +0300

    commented out init

--- odoo_cheque_management/__init__.py
+++ odoo_cheque_management/__init__.py
@@ -10,12 +10,12 @@ from . import controllers
 from . import wizard
 
 
-def pre_init_check(cr):
-    from odoo.service import common
-    from odoo.exceptions import UserError
-    version_info = common.exp_version()
-    server_serie = version_info.get('server_serie')
-    if not 16.0 < float(server_serie) <= 17.0:
-        raise UserError(
-            'Module support Odoo series 17.0 found {}.'.format(server_serie))
-    return True
+# def pre_init_check(cr):
+#     from odoo.service import common
+#     from odoo.exceptions import UserError
+#     version_info = common.exp_version()
+#     # server_serie = version_info.get('server_serie')
+#     # if not 16.0 < float(server_serie) <= 17.0:
+#     #     raise UserError(
+#     #         'Module support Odoo series 17.0 found {}.'.format(server_serie))
+#     # return True
