6ff2065e9965fcd0541a8907cc09767d5dc98c4c
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-20 14:48:01 +0300

    1

--- hosp/models/patient.py
+++ hosp/models/patient.py
@@ -5,18 +5,18 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
    
-    name = fields.Char(string='Patient Name', required=True)
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender')
-    contact = fields.Char(string="Phone Number")
+    name = fields.Char(string='Patient Name', required=True Tracking=True)
+    age = fields.Integer(string='Age' Tracking=True)
+    gender = fields.Selection([('male', 'Male'), ('female', 'Female')], string='Gender'Tracking=True)
+    contact = fields.Char(string="Phone Number" Tracking=True)
     next_of_kin_name = fields.Char(string="Next of Kin")
     next_of_kin_contact = fields.Char(string="Next of Kin Phone Number")
-    identification_number = fields.Char(string="National ID")
-    insurance = fields.Selection([('Yes', 'Yes'), ('No', 'No')], string='Are you insured?')
-    insurance_number = fields.Integer(string="Insurance Number")
-    weight = fields.Float(string="Weight")
+    identification_number = fields.Char(string="National ID" Tracking=True)
+    insurance = fields.Selection([('Yes', 'Yes'), ('No', 'No')], string='Are you insured?'Tracking=True)
+    insurance_number = fields.Integer(string="Insurance Number" Tracking=True)
+    weight = fields.Float(string="Weight" Tracking=True)
     active = fields.Boolean(string="Active", default='True')
-    category = fields.Selection([('Patient', 'patient'), ('Doctor', 'doctor')], string='Category')
+    category = fields.Selection([('Patient', 'patient'), ('Doctor', 'doctor')], string='Category'Tracking=True)
 
 
 
