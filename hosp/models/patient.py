0d572d41862f639b1991f3deffc81f963872efe8
joe muraya <75666813+murayajoe@users.noreply.github.com>
2025-02-20 12:13:49 +0300

    1

--- hosp/models/patient.py
+++ hosp/models/patient.py
@@ -3,7 +3,7 @@ from odoo import models, fields
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Hospital Patient'
-    _inherrit = 'mail.thread', 'mail.activity.mixin'
+    _inherrit = ['mail.thread', 'mail.activity.mixin']
 
     name = fields.Char(string='Patient Name', required=True)
     age = fields.Integer(string='Age')
